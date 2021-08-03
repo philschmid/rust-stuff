698177c11030afa46fb87c9bef68611bc6458073
philschmid <schmidphilipp1995@gmail.com>
2021-08-03 16:31:21 +0200

    rusty it a more

--- string_manipulation/src/main.rs
+++ string_manipulation/src/main.rs
@@ -1,19 +1,29 @@
 use url::{ParseError, Position, Url};
+#[derive(Debug)]
+struct Repository {
+    name: String,
+    filter: Option<String>,
+}
 
 fn main() -> Result<(), ParseError> {
-    let hf_uri = String::from("hf://philschmid/infinity-sentiment//infinity/config.json");
+    // let hf_uri = String::from("hf://philschmid/infinity-sentiment//infinity/config.json");
+    let hf_uri = String::from("hf://philschmid/infinity-sentiment");
     let parsed_hf_uri = Url::parse(hf_uri.as_str())?;
     let uri_without_schema: &str = &parsed_hf_uri[Position::BeforeHost..];
 
-    let filter = match uri_without_schema.contains("//") {
+    let repository: Repository = match uri_without_schema.contains("//") {
         true => {
             let url_split: Vec<&str> = uri_without_schema.split("//").collect();
-            url_split[1]
+            Repository {
+                name: String::from(url_split[0]),
+                filter: Some(String::from(url_split[1])),
+            }
         }
-        _ => "",
+        _ => Repository {
+            name: String::from(uri_without_schema),
+            filter: None,
+        },
     };
-    println!("filter: {}", filter);
-    println!("uri_without_schema: {}", uri_without_schema);
 
     Ok(())
 }
