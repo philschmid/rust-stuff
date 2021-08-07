8c0019e35678019cc42be1a7d4260abb1948fef9
philschmid <schmidphilipp1995@gmail.com>
2021-08-07 20:37:09 +0200

    aws lambda

---
+++ aws_lambda_rust/src/main.rs
@@ -0,0 +1,33 @@
+use lambda::{handler_fn, Context};
+use serde_json::Value;
+
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda::run(handler_fn(handler)).await?;
+    Ok(())
+}
+
+async fn handler(event: Value, _: Context) -> Result<Value, Error> {
+    Ok(event)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[tokio::test]
+    async fn handler_handles() {
+        let event = json!({
+            "answer": 42
+        });
+        assert_eq!(
+            handler(event.clone(), Context::default())
+                .await
+                .expect("expected Ok(_) value"),
+            event
+        )
+    }
+}
