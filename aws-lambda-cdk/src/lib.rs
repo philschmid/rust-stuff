d410378dc759f49d4cb85bb45cf706bd46ee5e14
Philipp Schmid <schmidphilipp1995@gmail.com>
2021-10-25 00:31:46 +0200

    added test

--- aws-lambda-cdk/src/lib.rs
+++ aws-lambda-cdk/src/lib.rs
@@ -1,8 +1,9 @@
 use lambda_http::{
     lambda_runtime::{Context, Error},
-    IntoResponse, Request, RequestExt, Response,
+    IntoResponse, Request, RequestExt,
 };
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 struct Human {
@@ -19,11 +20,15 @@ struct ResponseBody {
 
 pub async fn handler(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
     log::info!("{:?}", request);
+    println!("{:?}", request);
 
     // deserialize body
     let body: Human = match request.payload() {
         Ok(body) => body.unwrap(),
-        Err(err) => Err(err)?,
+        Err(err) => {
+            log::error!("{:?}", err);
+            return Ok(json!({"error":&err.to_string()}).into_response());
+        }
     };
     // calculate born year
     let born: u16 = 2021 - (body.age as u16);
@@ -42,7 +47,7 @@ pub async fn handler(request: Request, _: Context) -> Result<impl IntoResponse, 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::json;
+    use lambda_http::http::{self, HeaderValue};
 
     #[tokio::test]
     async fn test_lambda_handler() {
@@ -50,17 +55,17 @@ mod tests {
             "name":"Philipp",
             "age":28
         }"#;
-        let request = Request::new(lambda_http::Body::from(data));
-        let data = json!(
-        {
-            "name":"Philipp",
-            "age":28
-        });
-        let expected = data.into_response();
+        let mut request = Request::new(lambda_http::Body::from(data));
+        request.headers_mut().insert(
+            http::header::CONTENT_TYPE,
+            HeaderValue::from_static("application/json"),
+        );
+
         let response = handler(request, Context::default())
             .await
             .expect("expected Ok(_) value")
             .into_response();
-        assert_eq!(response.body(), expected.body())
+
+        assert_eq!(response.status(), 200)
     }
 }
