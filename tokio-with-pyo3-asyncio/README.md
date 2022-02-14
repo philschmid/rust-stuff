14c5ed0998f99984c1d560505efbd55cdbd4d7c6
philschmid <schmidphilipp1995@gmail.com>
2022-02-14 16:34:39 +0000

    added results and tests gor gpu

--- tokio-with-pyo3-asyncio/README.md
+++ tokio-with-pyo3-asyncio/README.md
@@ -1,5 +1,86 @@
 # How to work with Tokio & Asyncio ehm i mean Rust and Python 
 
+
+## Getting started
+
+```bash
+cargo run
+```
+send request
+```Bash
+curl --request POST \
+  --url http://localhost:3000/predict \
+  --header 'Content-Type: application/json' \
+  --data '{"inputs":"I like you"}'
+```
+
+## Run Test
+
+```bash
+cargo run --release
+```
+
+send load
+
+```Bash
+hey -n 1000 -m POST -H 'Content-Type: application/json' -d '{	"inputs": "I love you. I like you. I am your friend."}' http://127.0.0.1:3000/predict
+```
+**Result CPU:**
+
+```bash
+Summary:
+  Total:        30.9108 secs
+  Slowest:      1.5758 secs
+  Fastest:      0.1874 secs
+  Average:      1.5374 secs
+  Requests/sec: 32.3512
+  
+  Total data:   40000 bytes
+  Size/request: 40 bytes
+
+Response time histogram:
+  0.187 [1]     |
+  0.326 [5]     |
+  0.465 [0]     |
+  0.604 [0]     |
+  0.743 [0]     |
+  0.882 [0]     |
+  1.020 [0]     |
+  1.159 [0]     |
+  1.298 [0]     |
+  1.437 [0]     |
+  1.576 [994]   |■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+```
+
+
+**Result GPU:**
+
+```bash
+
+Summary:
+  Total:        4.1857 secs
+  Slowest:      0.2179 secs
+  Fastest:      0.0464 secs
+  Average:      0.2075 secs
+  Requests/sec: 238.9092
+  
+  Total data:   40000 bytes
+  Size/request: 40 bytes
+
+Response time histogram:
+  0.046 [1]     |
+  0.064 [9]     |
+  0.081 [0]     |
+  0.098 [0]     |
+  0.115 [0]     |
+  0.132 [0]     |
+  0.149 [0]     |
+  0.166 [0]     |
+  0.184 [0]     |
+  0.201 [0]     |
+  0.218 [990]   |■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+```
+
 ## Resources 
 
 * [My Github Issue](https://github.com/awestlake87/pyo3-asyncio/issues/60)
--- tokio-with-pyo3-asyncio/src/main.rs
+++ tokio-with-pyo3-asyncio/src/main.rs
@@ -5,8 +5,7 @@ type PyFunction = Py<PyAny>;
 use tower::ServiceBuilder;
 
 use axum::{
-    extract::Extension, http::Method, response::IntoResponse, routing::post, AddExtensionLayer,
-    Json, Router,
+    extract::Extension, response::IntoResponse, routing::post, AddExtensionLayer, Json, Router,
 };
 use serde::{Deserialize, Serialize};
 
@@ -26,39 +25,17 @@ struct TextClassificationResponse {
 }
 
 async fn predict_route(payload: PredictRequest, handler: Py<PyAny>) -> impl IntoResponse {
-    println!("{:?}", handler);
     let fut = Python::with_gil(|py| {
         let coro = handler.call1(py, (payload.inputs.as_str(),)).unwrap();
         pyo3_asyncio::tokio::into_future(coro.as_ref(py))
     })
     .unwrap();
-    println!("no infrence yet");
     let res = fut.await.unwrap();
 
     let res = Python::with_gil(|py| -> PyResult<Vec<TextClassificationResponse>> {
         res.extract::<Vec<TextClassificationResponse>>(py)
     });
     Json(res.unwrap())
-}
-
-// async fn predict_route(
-//     Json(payload): Json<PredictRequest>,
-//     Extension(handler): Extension<Py<PyAny>>,
-// ) -> impl IntoResponse {
-async fn handle(input: &str, handler: &PyFunction) -> Vec<TextClassificationResponse> {
-    let fut = Python::with_gil(|py| {
-        let coro = handler.call1(py, (input,)).unwrap();
-        pyo3_asyncio::tokio::into_future(coro.as_ref(py))
-    })
-    .unwrap();
-    println!("no infrence yet");
-    println!("{:?}", handler);
-    let res = fut.await.unwrap();
-
-    let res = Python::with_gil(|py| -> PyResult<Vec<TextClassificationResponse>> {
-        res.extract::<Vec<TextClassificationResponse>>(py)
-    });
-    res.unwrap()
 }
 
 #[pyo3_asyncio::tokio::main]
@@ -74,10 +51,6 @@ async fn main() -> PyResult<()> {
         handler.extract().unwrap()
     });
     println!("model loaded");
-
-    let res = handle("I like you. I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.I like you.", &pipeline);
-    println!("do i come before infernece?");
-    println!("{:?}", res.await);
 
     let middleware_stack = ServiceBuilder::new().layer(AddExtensionLayer::new(pipeline));
 
